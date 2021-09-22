ae8820fb49f2c9f9b87a890dfd2977bdcc6758a6
Phatorn <47255009+Phatorn@users.noreply.github.com>
2021-09-23 00:09:52 +0700

    Add files via upload

---
+++ ProgrammingProblem_w8_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+	char str[50], rstr[50], *p;
+	int i, count = 0;
+	p = str;
+	scanf("%s", &str);
+	while (*p != '\0') 
+		p++;
+	p--;
+	while (p >= str) {
+		if(*p == 'a') printf("0");
+		else if(*p == 'e') printf("1");
+		else if(*p == 'i') printf("2");
+		else if(*p == 'o') printf("3");
+		else if(*p == 'u') printf("4");
+		else printf("%c", *p);
+		p--;
+	}
+	printf("aca");
+	return 0;
+}
